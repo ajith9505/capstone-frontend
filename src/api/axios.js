c499ba0c4fc23ad4760a34e090a2ac8b7b42c69a
ajith9505 <ajith.aj987@gmail.com>
2024-03-27 19:24:52 +0530

    update 001

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3500',
+    baseURL: 'https://pettycash-manager1995.onrender.com',
 })
 
 export const axiosPrivate = axios.create({
-  baseURL: 'http://localhost:3500',
+  baseURL: 'https://pettycash-manager1995.onrender.com',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 });
