d8b89b0e2ae38cedd5ceb37be1e24aec4ef55e74
ajith9505 <ajith.aj987@gmail.com>
2024-03-28 05:08:12 +0530

    update 003

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
             <div className="dash-container">
                 <Outlet />
             </div>
-            <DashFooter />
+            {/* <DashFooter /> */}
         </>
     )
 }
